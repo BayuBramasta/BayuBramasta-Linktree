5a69d8717921c63ca674d0b0fb48bdadb8d89ad1
Bayu Bramasta <162079048+BayuBramasta@users.noreply.github.com>
2024-03-13 23:37:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-VANTA.BIRDS({
+VANTA.RINGS({
   el: "#VANTA",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 200.00,
-	minWidth: 200.00,
-  scale: 1.00,
-	scaleMobile: 1.00,
-  backgroundAlpha: 0.00
-  })
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  backgroundAlpha: 0.0,
+});
+
+setTimeout(() => {
+  const content = document.querySelector(".content");
+  content.style.opacity = 1;
+  content.style.filter = "blur(0px)";
+}, 1000);
